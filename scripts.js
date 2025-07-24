eba905e1117eb45909e26cc9bb5c9516af05a9f9
Sonaliyadav17 <144614845+Sonaliyadav17@users.noreply.github.com>
2025-07-25 02:04:09 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x202b3a, // more subtle node color
+  backgroundColor: 0x0b0c10, // dark but clean
+  points: 8.0,      // fewer points
+  maxDistance: 20.0, // denser network
+  spacing: 18.0,     // more space
+})
